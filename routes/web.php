27cb5e97abbd9675a6abf5b00ae45f75fb56cb08
3A432081陳紹田\admin <s707071127@gmail.com>
2017-10-25 20:11:14 +0800

    修改參數成選擇性

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ Route::get('/', function() {
     return 'welcome';
 });
 Route::get('/', function() {
-    return view('welcome');
-});
-Route::get('/', function() {
     return redirect('welcome');
 });
 Route::get('hello/{name}', function($name) {
     return 'Hello, '.$name;
+});
+Route::get('hello/{name?}', function($name = 'Everybody') {
+return 'Hello, '.$name;
 });

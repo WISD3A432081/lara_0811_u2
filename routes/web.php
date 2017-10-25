4a2fb21a95916e72ff3b870f9fa96514eb8d1eef
3A432081陳紹田\admin <s707071127@gmail.com>
2017-10-25 20:18:16 +0800

    修改 Route 裡的路徑

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,6 @@ Route::get('hello/{name}', function($name) {
 Route::get('hello/{name?}', function($name = 'Everybody') {
 return 'Hello, '.$name;
 });
-Route::get('hello/{name?}', ['as' => 'hello.index', function($name = 'Everybody') {
-    return 'Hello, '.$name;
-} ] );
+Route::get(' say /{name?}', ['as' => 'hello.index', function($name = 'Everybody') {
+    return view('welcome');
+}]);

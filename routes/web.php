3f9fd3f109beb5068e1ca4ff8e2f419df51d18f0
3A432081 <s707071127@gmail.com>
2017-12-01 22:58:36 +0800

    註解前面的route練習

--- routes/web.php
+++ routes/web.php
@@ -11,28 +11,27 @@
 |
 */
 
-Route::get('/', function() {
-    return 'welcome';
-});
-Route::get('/', function() {
-    return redirect('welcome');
-});
-Route::get('hello/{name}', function($name) {
-    return 'Hello, '.$name;
-});
-Route::get('hello/{name?}', function($name = 'Everybody') {
-return 'Hello, '.$name;
-});
-Route::get(' say /{name?}', ['as' => 'hello.index', function($name = 'Everybody') {
-    return view('welcome');
-}]);
-Route::get('dashboard', function() {
-    return 'dashboard';
-});
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('dashboard', function() {
+//Route::get('/', function() {
+//    return 'welcome';
+//});
+//Route::get('/', function() {
+//    return redirect('welcome');
+//});
+//Route::get('hello/{name}', function($name) {
+//    return 'Hello, '.$name;
+//});
+//Route::get('hello/{name?}', function($name = 'Everybody') {
+//return 'Hello, '.$name;
+//});
+//Route::get(' say /{name?}', ['as' => 'hello.index', function($name = 'Everybody') {
+//    return view('welcome');
+//}]);
+//Route::get('dashboard', function() {
+//    return 'dashboard';
+//});
+Route::group(['prefix' => 'admin'],function () {
+    Route::get('dashboard', function () {
         return 'admin dashboard';
     });
 });
-Route::get('/', ['as' => 'home.index', 'uses' =>
-    'HomeController@index']);
+Route::get('/ ',['as' => 'home.index', 'uses' => 'HomeController@index']);
